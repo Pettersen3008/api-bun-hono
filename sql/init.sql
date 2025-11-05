23e1fce9ecf0b8b59e387a05a52a3713bb03485f
Sander Pettersen <48487730+Pettersen3008@users.noreply.github.com>
2025-11-05 13:46:17 +0100

    added init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE IF NOT EXISTS todos;
-
-CREATE TABLE IF NOT EXISTS todos (
+CREATE TABLE IF NOT EXISTS public.todos (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
